71c2b0398bf0968f71ab1f6f0b90e8c3128b3af4
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-02-23 20:47:11 +0100

    seo update

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.veralyssa.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.8,
+};
